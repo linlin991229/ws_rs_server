a3d888b235b6bf3d08719d6767a9bf23739a976c
xianlin <xianlin@intasect.com.cn>
2024-02-06 09:28:43 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use ws::listen;
 fn main() {
     // 监听地址
     if let Err(error) = listen("127.0.0.1:8080",|out|{
-        // 处理程序需要获取out的所有权，因此我们使用move
+        // 处理程序需要获取out的所有权，使用move
         move |msg|{
             // 处理在此连接上接收的消息
             println!("服务器收到消息 '{}'. ", msg);
